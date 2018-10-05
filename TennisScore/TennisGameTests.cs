d7514f670c4bb22ddfd109aade8bbca5b0771592
Janet Wang <ad.janetwang@gmail.com>
2018-10-06 05:25:20 +0800

    2. pass test:Fifteen Love

--- TennisScore/TennisGameTests.cs
+++ TennisScore/TennisGameTests.cs
@@ -17,6 +17,13 @@ namespace TennisScore
             ScoreResultShouldBe("Love All");
         }
 
+        [TestMethod]
+        public void Fifteen_Love()
+        {
+            GivenGame(firstPlayerScore:1 , secondPlayerScore: 0);
+            ScoreResultShouldBe("Fifteen Love");
+        }
+
         private void ScoreResultShouldBe(string expected)
         {
             var scoreResult = _tennisGame.ScoreResult(AnyGameId);
