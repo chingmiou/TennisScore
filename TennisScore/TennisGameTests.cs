3ce6d04b506e6eb0d6aca3f4ad1b3bbb455aa9d3
Janet Wang <ad.janetwang@gmail.com>
2018-10-06 05:20:42 +0800

    1. pass test: love all ,and refactor testing

---
+++ TennisScore/TennisGameTests.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NSubstitute;
+
+namespace TennisScore
+{
+    [TestClass]
+    public class TennisGameTests
+    {
+        private const int AnyGameId = 1;
+        private IRepository<Game> _repository = Substitute.For<IRepository<Game>>();
+        private TennisGame _tennisGame;
+
+        [TestMethod]
+        public void Love_All()
+        {
+            GivenGame(firstPlayerScore: 0, secondPlayerScore: 0);
+            ScoreResultShouldBe("Love All");
+        }
+
+        private void ScoreResultShouldBe(string expected)
+        {
+            var scoreResult = _tennisGame.ScoreResult(AnyGameId);
+            Assert.AreEqual(expected, scoreResult);
+        }
+
+        private void GivenGame(int firstPlayerScore, int secondPlayerScore)
+        {
+            _repository.GetGame(AnyGameId).Returns(new Game {Id = AnyGameId, FirstPlayerScore = firstPlayerScore, SecondPlayerScore =secondPlayerScore });
+        }
+
+        [TestInitialize]
+        public void TestInit()
+        {
+            _tennisGame = new TennisGame(_repository);
+        }
+    }
+}
