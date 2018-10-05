437a242c4194e93330ff5b32da0631b9928a61f2
Janet Wang <ad.janetwang@gmail.com>
2018-10-06 06:23:55 +0800

    4. refactor: intrudce scoreLookup

--- TennisScore/TennisGame.cs
+++ TennisScore/TennisGame.cs
@@ -1,10 +1,14 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace TennisScore
 {
     public class TennisGame
     {
         private readonly IRepository<Game> _repo;
+
+        private Dictionary<int, string> _scoreLookup = new Dictionary<int , string>{{1, "Fifteen"} ,{2, "Thirty"}
+        };
 
         public TennisGame(IRepository<Game> repo)
         {
@@ -14,6 +18,10 @@ namespace TennisScore
         public string ScoreResult(int gameId)
         {
             var game = this._repo.GetGame(gameId);
+            if (game.FirstPlayerScore > 0)
+            {
+                return _scoreLookup[game.FirstPlayerScore] + " Love";
+            }
 
             if (game.FirstPlayerScore == 2)
             {
